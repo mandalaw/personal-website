2e47800f3d440e6511d95771df387c64017b3db7
mandalaw <al.mandalawi@mail.utoronto.ca>
2023-03-10 22:14:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -136,8 +136,11 @@ out.addEventListener("click", (event)=>{
   event.preventDefault();
   localStorage.clear();
   loginBtn.textContent = "Sign In";
-  out.classList.remove("d-none")
-  location.reload();
+  out.classList.remove("d-none");
+  out.style.display = "none";
+  info.style.display = "none";
+  sign.classList.remove("d-none");
+  sign.classList.add("g_id_signin");
   // g_id_onload.style.display = "display";
 
   // Hide navbar items
